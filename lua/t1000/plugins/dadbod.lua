ef080e2e331b83e249da9d14ba5944b839829d6f
Schrotti17 <thom.st@protonmail.com>
2024-08-27 22:44:57 +0200

    Dadbod

--- lua/t1000/plugins/dadbod.lua
+++ lua/t1000/plugins/dadbod.lua
@@ -1,5 +1,7 @@
 return {
-  "tpope/vim-dadbod",
-  "kristijanhusak/vim-dadbod-completion",
-  "kristijanhusak/vim-dadbod-ui",
-}    
+  'kristijanhusak/vim-dadbod-ui',
+  dependencies = {
+    { 'tpope/vim-dadbod', lazy = true },
+    { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+  },
+}
